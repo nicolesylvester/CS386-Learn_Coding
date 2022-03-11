a26688abb7637e70055f52e6a5965fd5455c5df6
ind9-nau <93492981+ind9-nau@users.noreply.github.com>
2022-03-10 20:49:12 -0700

    Getting new project commited

---
+++ implementation/codeHub/Pages/References.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace codeHub.Pages
+{
+    public class ReferencesModel : PageModel
+    {
+        private readonly ILogger<ReferencesModel> _logger;
+
+        public ReferencesModel(ILogger<ReferencesModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
