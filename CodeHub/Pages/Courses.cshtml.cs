60e52839ccab82b54eb275352455541e91bf7c6b
nicolesylvester <niccisylvester@gmail.com>
2022-03-18 16:31:09 -0700

    added tabs, changed models

---
+++ CodeHub/Pages/Courses.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeHub.Pages
+{
+    public class CoursesModel : PageModel
+    {
+        private readonly ILogger<CoursesModel> _logger;
+
+        public CoursesModel(ILogger<CoursesModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
