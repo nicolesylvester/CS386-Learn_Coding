b9129c124da4a928ea8c4ae88b28d651b27bcc44
nicolesylvester <niccisylvester@gmail.com>
2022-04-12 15:25:53 -0700

    added navigation tabs in courses

---
+++ CodeHub/Pages/Python.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeHub.Pages
+{
+    public class PythonModel : PageModel
+    {
+        private readonly ILogger<PythonModel> _logger;
+
+        public PythonModel(ILogger<PythonModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CodeHub/Pages/WebDev.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeHub.Pages
+{
+    public class WebDevModel : PageModel
+    {
+        private readonly ILogger<WebDevModel> _logger;
+
+        public WebDevModel(ILogger<WebDevModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
