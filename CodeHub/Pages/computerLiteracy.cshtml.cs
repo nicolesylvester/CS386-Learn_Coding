2c27fc5e580bb477098f852484bf153225df7c04
nasyav <nv339@nau.edu>
2022-04-12 20:51:39 -0700

    updated buttons

---
+++ CodeHub/Pages/computerLiteracy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeHub.Pages
+{
+    public class computerLitModel : PageModel
+    {
+        private readonly ILogger<computerLitModel> _logger;
+
+        public computerLitModel(ILogger<computerLitModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
