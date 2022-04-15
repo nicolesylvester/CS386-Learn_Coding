309a2f125bedf9046a123c8ba988898c1324efe6
baileyrosato <bdr245@nau.edu>
2022-04-14 17:04:29 -0700

    added pyhton course instructions

---
+++ CodeHubTests/CoursesModelTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeHub.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeHub.Pages.Tests
+{
+    [TestClass()]
+    public class CoursesModelTests
+    {
+        [TestMethod()]
+        public void CoursesModelTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
